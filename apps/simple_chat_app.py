337fda3be0f166182483244a2f5a9e56b7d94208
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-04-30 20:52:03 +0200

    Add simple_chat_app.py

---
+++ apps/simple_chat_app.py
@@ -0,0 +1,70 @@
+# WS server example that synchronizes state across clients
+
+import asyncio
+import json
+import logging
+import websockets
+
+logging.basicConfig()
+
+THREAD = []
+
+USERS = set()
+
+
+def thread_event():
+    return json.dumps({"type": "thread", "last_message": THREAD[-1] if 0 < len(THREAD) else None})
+
+
+def users_event(new_user):
+    return json.dumps({"type": "users", "count": len(USERS),
+                       "count_change_message": "user joined the chat" if new_user else "user left the chat"})
+
+
+async def notify_thread():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = thread_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def notify_users(new_user):
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = users_event(new_user=new_user)
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    # Add user to count on login
+    USERS.add(websocket)
+    await notify_users(new_user=True)
+
+
+async def unregister(websocket):
+    # Delete user from count on logout
+    USERS.remove(websocket)
+    await notify_users(new_user=False)
+
+
+async def chat(websocket, path):
+    # register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(thread_event())
+        async for message in websocket:
+            data = json.loads(message)
+            if data["message"]:
+                message = data["message"]
+                logging.info("message '{}' received", message)
+                THREAD.append(message)
+                print(THREAD)
+                await notify_thread()
+            else:
+                logging.error("unsupported event: {}", data)
+    finally:
+        await unregister(websocket)
+
+
+start_server = websockets.serve(chat, "localhost", 6789)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
