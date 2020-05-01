597a66060ece146d8a874e29a2c802f604d36317
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-05-01 14:06:13 +0200

    Add history to simple_chat_app

--- apps/simple_chat_app.py
+++ apps/simple_chat_app.py
@@ -12,6 +12,10 @@ THREAD = []
 USERS = set()
 
 
+def history_event():
+    return json.dumps({"type": "history", "thread_history": THREAD if 0 < len(THREAD) else None})
+
+
 def thread_event():
     return json.dumps({"type": "thread", "last_message": THREAD[-1] if 0 < len(THREAD) else None})
 
@@ -21,14 +25,20 @@ def users_event(new_user):
                        "count_change_message": "user joined the chat" if new_user else "user left the chat"})
 
 
+async def notify_history(user):
+    if USERS:
+        message = history_event()
+        await asyncio.create_task(user.send(message))
+
+
 async def notify_thread():
-    if USERS:  # asyncio.wait doesn't accept an empty list
+    if USERS:
         message = thread_event()
         await asyncio.wait([user.send(message) for user in USERS])
 
 
 async def notify_users(new_user):
-    if USERS:  # asyncio.wait doesn't accept an empty list
+    if USERS:
         message = users_event(new_user=new_user)
         await asyncio.wait([user.send(message) for user in USERS])
 
@@ -36,6 +46,7 @@ async def notify_users(new_user):
 async def register(websocket):
     # Add user to count on login
     USERS.add(websocket)
+    await notify_history(websocket)
     await notify_users(new_user=True)
 
 
@@ -49,14 +60,12 @@ async def chat(websocket, path):
     # register(websocket) sends user_event() to websocket
     await register(websocket)
     try:
-        await websocket.send(thread_event())
         async for message in websocket:
             data = json.loads(message)
             if data["message"]:
                 message = data["message"]
                 logging.info("message '{}' received", message)
                 THREAD.append(message)
-                print(THREAD)
                 await notify_thread()
             else:
                 logging.error("unsupported event: {}", data)
